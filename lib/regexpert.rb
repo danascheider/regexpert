5e33f3f2faf0267bb27ad7df700a8820d5558309
danascheider <dscheider@renewfinancial.com>
2016-04-16 12:50:13 -0700

    More naive implementations of stuff

--- lib/regexpert.rb
+++ lib/regexpert.rb
@@ -3,10 +3,11 @@ module Regexpert
     def generate(regex)
       regex = regex.inspect[1..-2]
 
-      if regex.match(/\./)
+      if regex.match(/\./) || regex.match(/\\w/)
         "a"
       elsif regex.match(/\\d/)
         "1"
+      elsif regex.mat
       end
     end
   end
--- spec/lib/regexpert_spec.rb
+++ spec/lib/regexpert_spec.rb
@@ -19,6 +19,15 @@ describe Regexpert do
     context "character types" do
       context "numbers" do
         let(:expression) { /\d+/ }
+
+        it "generates a matching string" do
+          expect(Regexpert.generate(expression)).to match expression
+        end
+      end
+
+      context "word characters" do
+        let(:expression) { /\w+/ }
+
         it "generates a matching string" do
           expect(Regexpert.generate(expression)).to match expression
         end
