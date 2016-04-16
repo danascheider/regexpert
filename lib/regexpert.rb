e410b05b1bb1c8f1aa650954ef9779b3d72eb9c8
danascheider <dscheider@renewfinancial.com>
2016-04-16 12:47:37 -0700

    Handle an additional case

--- lib/regexpert.rb
+++ lib/regexpert.rb
@@ -1,7 +1,13 @@
 module Regexpert
   class << self
     def generate(regex)
-      ""
+      regex = regex.inspect[1..-2]
+
+      if regex.match(/\./)
+        "a"
+      elsif regex.match(/\\d/)
+        "1"
+      end
     end
   end
 end
--- spec/lib/regexpert_spec.rb
+++ spec/lib/regexpert_spec.rb
@@ -7,5 +7,22 @@ describe Regexpert do
         expect(Regexpert.generate(expression)).to match expression
       end
     end
+
+    context "at least one character" do
+      let(:expression) { /.+/ }
+
+      it "generates a matching string" do
+        expect(Regexpert.generate(expression)).to match expression
+      end
+    end
+
+    context "character types" do
+      context "numbers" do
+        let(:expression) { /\d+/ }
+        it "generates a matching string" do
+          expect(Regexpert.generate(expression)).to match expression
+        end
+      end
+    end
   end
 end
