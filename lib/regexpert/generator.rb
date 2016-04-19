dd5736802eadca86537734f4f63dc17dc4e2cf95
danascheider <dscheider@renewfinancial.com>
2016-04-19 10:36:59 -0700

    Delete commented out code

--- lib/regexpert/generator.rb
+++ lib/regexpert/generator.rb
@@ -10,12 +10,8 @@ module Regexpert
       expression = regex.to_s
       str   = ""
 
-      regex.split.each do |el|
-        if expr = Regexpert::Regex.matchers.find {|exp, val| el.match(exp) }
-          str << expr.last
-        else
-          str << el
-        end
+      str << regex.split.each do |el|
+        expr = Regexpert::Regex.matchers.find {|exp, val| el.match(exp) } ? expr.last : el
       end
 
       str
