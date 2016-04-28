405db227b06edf45c752afcba20a32d6fdabceae
danascheider <dscheider@renewfinancial.com>
2016-04-28 16:45:12 -0700

    Make sure POSIX bracket expressions are not treated as groupings

--- lib/regxing/regex.rb
+++ lib/regxing/regex.rb
@@ -38,8 +38,12 @@ module RegXing
         [ /^\^/, /\$$/ ]
       end
 
+      def posix
+        /\[\[\:[a-z]{5,6}\:\]\]/
+      end
+
       def is_posix?(string)
-        string.match(/^\[\[\:[a-z]{5,6}\:\]\]$/) ? true : false
+        string.match(posix) ? true : false
       end
 
       def process_count_indicator(indicator)
@@ -103,7 +107,7 @@ module RegXing
     end
 
     def extract_groupings
-      to_s.split(/[\(\)\[\]]/).reject {|str| str == "" }
+      to_s.split(/[\(\)\[)\]](?![\[\]])(?<![\[\]])/).reject {|str| str == "" }
     end
 
     def is_anchor(char)
